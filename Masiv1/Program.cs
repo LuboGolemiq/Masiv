ad0d888e8aaba2bd93fbcaf86131d885c2a68c87
LuboGolemiq <lubomirpalazov19@gmail.com>
2024-04-28 23:36:00 +0300

    kop

---
+++ Masiv1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Masiv1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int rows = int.Parse(Console.ReadLine());
+          int cols = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[rows,cols] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int row = 0 ;row < row; row++)
+            {
+                for (int col = 0; col < col; col++)
+                {
+                    Console.WriteLine(matrix[row,col]+" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
